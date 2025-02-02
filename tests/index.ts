beb1efea72d1b73b984eb733d8711b4a9d7935c9
Hiroki Osame <hiroki.osame@gmail.com>
2025-02-02 11:36:20 +0900

    wip

--- tests/index.ts
+++ tests/index.ts
@@ -4,6 +4,6 @@ import { eslintVersions } from './utils/eslint-versions.js';
 for (const eslint of eslintVersions) {
 	describe(eslint, ({ runTestSuite }) => {
 		runTestSuite(import('./specs/basic-usage.js'), eslint);
-		// runTestSuite(import('./specs/insert-disable-comment/index.js'), eslint);
+		runTestSuite(import('./specs/insert-disable-comment/index.js'), eslint);
 	});
 }
